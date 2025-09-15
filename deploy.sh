f4560426b774b3ab5bf0e16bf4d2febd1669d34d
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2025-09-15 16:50:40 +0200

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cargo build --release --target x86_64-unknown-linux-musl
 rm -rf docker/rootfs
 mkdir docker/rootfs
 
-cp target/x86_64-unknown-linux-musl/release/server docker/rootfs
+cp target/x86_64-unknown-linux-musl/release/docserver docker/rootfs
 cp -r templates docker/rootfs
 
 IMAGE=embassy.dev/docserver:$(date '+%Y%m%d%H%M%S')
